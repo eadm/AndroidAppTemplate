c27bff838576d7ca4dc0e322e38216c71ec85722
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-11-04 01:45:01 +0300

    add UTCDateSerializer to di

--- app/src/main/java/ru/nobird/template/view/injection/network/NetworkModule.kt
+++ app/src/main/java/ru/nobird/template/view/injection/network/NetworkModule.kt
@@ -4,13 +4,16 @@ import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFact
 import dagger.Module
 import dagger.Provides
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.contextual
 import okhttp3.Interceptor
 import okhttp3.MediaType
 import okhttp3.OkHttpClient
-import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import ru.nobird.template.remote.base.model.Config
+import ru.nobird.template.remote.base.serialization.UTCDateSerializer
+import ru.nobird.template.view.injection.app.ApplicationScope
 import ru.nobird.template.view.injection.qualifiers.DebugInterceptors
 
 @Module(includes = [NetworkUtilModule::class])
@@ -23,7 +26,7 @@ abstract class NetworkModule {
             config: Config,
             @DebugInterceptors
             debugInterceptors: Set<@JvmSuppressWildcards Interceptor>,
-            jsonConverterFactory: Converter.Factory
+            json: Json
         ): Retrofit {
             val okHttpBuilder = OkHttpClient.Builder()
             debugInterceptors.forEach { okHttpBuilder.addNetworkInterceptor(it) }
@@ -31,15 +34,20 @@ abstract class NetworkModule {
             return Retrofit.Builder()
                 .baseUrl(config.baseUrl)
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .addConverterFactory(jsonConverterFactory)
+                .addConverterFactory(json.asConverterFactory(MediaType.get("application/json")))
                 .client(okHttpBuilder.build())
                 .build()
         }
 
         @Provides
         @JvmStatic
-        fun provideJsonFactory(): Converter.Factory =
-            Json { coerceInputValues = true } // todo add UTCAdapter
-                .asConverterFactory(MediaType.get("application/json"))
+        @ApplicationScope
+        fun provideJson(): Json =
+            Json {
+                coerceInputValues = true
+                serializersModule = SerializersModule {
+                    contextual(UTCDateSerializer())
+                }
+            }
     }
 }
