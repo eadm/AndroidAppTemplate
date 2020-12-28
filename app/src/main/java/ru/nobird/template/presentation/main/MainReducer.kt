2e43cc18bae4c696bea0d9974373244ffbd02f3e
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-12-28 13:26:41 +0300

    lint

--- app/src/main/java/ru/nobird/template/presentation/main/MainReducer.kt
+++ app/src/main/java/ru/nobird/template/presentation/main/MainReducer.kt
@@ -1,14 +1,14 @@
 package ru.nobird.template.presentation.main
 
 import ru.nobird.android.presentation.redux.reducer.StateReducer
-import ru.nobird.template.presentation.main.MainFeature.State
-import ru.nobird.template.presentation.main.MainFeature.Message
 import ru.nobird.template.presentation.main.MainFeature.Action
+import ru.nobird.template.presentation.main.MainFeature.Message
+import ru.nobird.template.presentation.main.MainFeature.State
 import javax.inject.Inject
 
 class MainReducer
 @Inject
-constructor(): StateReducer<State, Message, Action> {
+constructor() : StateReducer<State, Message, Action> {
     override fun reduce(state: State, message: Message): Pair<State, Set<Action>> =
         when (message) {
             is Message.Init ->
