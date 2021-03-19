6c9209e20a6c76e207ec3f2531d0a3398ea56d74
RationalStargazer <rationalstargazer@gmail.com>
2021-03-19 13:11:55 +0300

    ActionDispatcherOptions improved a bit

--- app/src/main/java/ru/nobird/template/presentation/main/base/ActionDispatcherOptions.kt
+++ app/src/main/java/ru/nobird/template/presentation/main/base/ActionDispatcherOptions.kt
@@ -5,13 +5,19 @@ import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import ru.nobird.android.presentation.redux.dispatcher.CoroutineActionDispatcher
 
-class ActionDispatcherOptions : CoroutineActionDispatcher.ScopeConfigOptions {
-    override val actionParentScope: CoroutineScope? = null
+/**
+ * Null scope means ActionDispatcher can decide for itself
+ * (default behavior: MainDispatcher() will be used for both actionParentScope and messageParentScope)
+ */
+class ActionDispatcherOptions(
+    override val actionParentScope: CoroutineScope? = null,
+    override val messageParentScope: CoroutineScope? = null
+) : CoroutineActionDispatcher.ScopeConfigOptions {
+
     override val actionScopeExceptionHandler: CoroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
         Log.e("ActionScopeException", "ActionDispatcher: exception on action scope", throwable)
     }
 
-    override val messageParentScope: CoroutineScope? = null
     override val messageScopeExceptionHandler: CoroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
         Log.e("MessageScopeException", "ActionDispatcher: exception on message scope", throwable)
         throw throwable // rethrow
