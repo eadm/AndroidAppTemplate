ff7cc0365f8b72c4bf894af6aad1bbdc97029a68
Ruslan Davletshin <smartmorefeed@gmail.com>
2021-11-05 23:08:02 +0300

    update cicerone

--- app/src/main/java/ru/nobird/template/presentation/main/MainActionDispatcher.kt
+++ app/src/main/java/ru/nobird/template/presentation/main/MainActionDispatcher.kt
@@ -1,9 +1,9 @@
 package ru.nobird.template.presentation.main
 
+import com.github.terrakok.cicerone.Router
 import ru.nobird.android.presentation.redux.dispatcher.CoroutineActionDispatcher
 import ru.nobird.template.domain.sample.interactor.SampleInteractor
 import ru.nobird.template.presentation.main.base.ActionDispatcherOptions
-import ru.terrakok.cicerone.Router
 import javax.inject.Inject
 
 class MainActionDispatcher
