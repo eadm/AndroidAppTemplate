c53308719c201fc905ba3039b46d2779c77ebd7d
Ruslan Davletshin <smartmorefeed@gmail.com>
2021-11-05 23:07:43 +0300

    version_catalogs: migrate to version catalogs & kts

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        jcenter()
+        google()
+        mavenLocal()
+
+    }
+    dependencies {
+        classpath(libs.plugin.kotlin)
+        classpath(libs.plugin.kotlinSerialization)
+        classpath(libs.plugin.android)
+        classpath(libs.plugin.dexcount)
+//        classpath gradlePlugins.kotlinSerizalizationPlugin
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        jcenter()
+        google()
+        maven {
+            url = uri("https://maven.pkg.github.com/eadm/AndroidKit")
+            credentials {
+                username = System.getenv("GITHUB_USER") ?: project.properties["GITHUB_USER"] as String?
+                password = System.getenv("GITHUB_PERSONAL_ACCESS_TOKEN") ?: project.properties["GITHUB_PERSONAL_ACCESS_TOKEN"] as String?
+            }
+        }
+        maven {
+            url = uri("https://maven.pkg.github.com/eadm/ktlint-rules")
+            credentials {
+                username = System.getenv("GITHUB_USER") ?: project.properties["GITHUB_USER"] as String?
+                password = System.getenv("GITHUB_PERSONAL_ACCESS_TOKEN") ?: project.properties["GITHUB_PERSONAL_ACCESS_TOKEN"] as String?
+            }
+        }
+        maven { url = uri("https://jitpack.io") }
+        mavenLocal()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
